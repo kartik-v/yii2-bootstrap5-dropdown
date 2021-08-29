e522b9b7cc2269aa62ee005c6ca8e1d05c146b06
Kartik Visweswaran <kartikv2@gmail.com>
2021-08-29 22:08:20 +0530

    Initial release

---
+++ CHANGE.md
@@ -0,0 +1,8 @@
+Change Log: `yii2-bootstrap5-dropdown`
+======================================
+
+## Version 1.0.0
+
+**Date:** 29-Aug-2021
+
+- Initial release
---
+++ src/DropdownAsset.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package   yii2-bootstrap5-dropdown
+ * @author    Kartik Visweswaran <kartikv2@gmail.com>
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2015 - 2020
+ * @version   1.0.0
+ */
+
+namespace kartik\bs5dropdown;
+
+use kartik\base\PluginAssetBundle;
+
+/**
+ * Dropdown asset bundle.
+ */
+class DropdownAsset extends PluginAssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $bsVersion = '5.x';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->setSourcePath(__DIR__ . '/assets');
+        $this->setupAssets('js', ['js/dropdown']);
+        $this->setupAssets('css', ['css/dropdown']);
+        parent::init();
+    }
+}
